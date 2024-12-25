0174917127d054a89252391032736ece8298bff8
AndrewRich10 <adrich@uci.edu>
2024-12-25 14:07:59 -0800

    fixed sawblade svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '',
   root: '',
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173, // Optional: specify a port
